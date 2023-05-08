86f42e0cb15efd2b113577e4866567934c62b25d
Girish <m.girishroy@gmail.com>
2023-05-08 16:43:17 +0530

    url changes in client

--- client/src/uitils/baseAxios.jsx
+++ client/src/uitils/baseAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://chats-app-71dq.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
